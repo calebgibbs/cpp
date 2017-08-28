99fc3541770950021f8965a87c10a2e94b6479b8
Caleb Gibbs <calebgibbs@Calebs-MBP.hub>
2017-08-28 20:44:35 +1200

    Making shapes with cout

---
+++ shapes.cpp
@@ -0,0 +1,19 @@
+#include<iostream> 
+
+#pragma hdrstop 
+using namespace std; 
+
+//---------------------------------------------------------------------------------------------------------------
+
+#pragma argsused 
+int main(int argc, char* argv[]){ 
+	cout << "* * * * *" << endl 
+	 	 << "*       *" << endl 
+	 	 << "*       *" << endl
+	 	 << "*       *" << endl
+	 	 << "* * * * *" << endl;
+
+
+	std::getchar();
+	return 0; 
+}
